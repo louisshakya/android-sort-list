911b9143eccf9a83c0d023768cf6b9bcac9079d5
louis7shakya <louis.shakya@huskers.unl.edu>
2022-06-08 18:31:31 -0500

    Added onClick listeners for the buttons to sort the list accordingly

--- app/src/main/java/com/louis/androidlistapp/MainActivity.java
+++ app/src/main/java/com/louis/androidlistapp/MainActivity.java
@@ -1,22 +1,29 @@
 package com.louis.androidlistapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewbinding.ViewBinding;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.louis.androidlistapp.databinding.ActivityMainBinding;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
 
     private ActivityMainBinding binding;
     private ArrayList<List> newList =new ArrayList<>();
+    private boolean isSortByListIDPressed, isSortByNamePressed, isSortByBothPressed = false;
 
     public ArrayList<List> getNewList() {
         return newList;
@@ -31,7 +38,76 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        setOnClickListeners();
         updateRecyclerView(newList);
+        fetchData();
+
+    }
+
+    public void setOnClickListeners() {
+        binding.listIdButtonId.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isSortByNamePressed = false;
+                isSortByBothPressed = false;
+                binding.nameButtonId.setBackgroundColor(ContextCompat.getColor(MainActivity.this,R.color.white));
+                binding.bothButtonId.setBackgroundColor(ContextCompat.getColor(MainActivity.this,R.color.white));
+                ArrayList<List> tempList = new ArrayList<>(getNewList());
+                if (isSortByListIDPressed) {
+                    isSortByListIDPressed = false;
+                    updateRecyclerView(tempList);
+                    binding.listIdButtonId.setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.white));
+                } else {
+                    isSortByListIDPressed = true;
+                    sortByListId(tempList);
+                    updateRecyclerView(tempList);
+                    binding.listIdButtonId.setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.purple_200));
+                }
+            }
+        });
+        binding.nameButtonId.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isSortByListIDPressed = false;
+                isSortByBothPressed = false;
+                binding.listIdButtonId.setBackgroundColor(ContextCompat.getColor(MainActivity.this,R.color.white));
+                binding.bothButtonId.setBackgroundColor(ContextCompat.getColor(MainActivity.this,R.color.white));
+                ArrayList<List> tempList = new ArrayList<>(getNewList());
+                if (isSortByNamePressed) {
+                    isSortByNamePressed = false;
+                    updateRecyclerView(tempList);
+                    binding.nameButtonId.setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.white));
+                } else {
+                    isSortByNamePressed = true;
+                    sortByName(tempList);
+                    updateRecyclerView(tempList);
+                    binding.nameButtonId.setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.purple_200));
+                }
+            }
+        });
+        binding.bothButtonId.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isSortByNamePressed = false;
+                isSortByListIDPressed = false;
+                binding.nameButtonId.setBackgroundColor(ContextCompat.getColor(MainActivity.this,R.color.white));
+                binding.listIdButtonId.setBackgroundColor(ContextCompat.getColor(MainActivity.this,R.color.white));
+                ArrayList<List> tempList = new ArrayList<>(getNewList());
+                if (isSortByBothPressed) {
+                    isSortByBothPressed = false;
+                    updateRecyclerView(tempList);
+                    binding.bothButtonId.setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.white));
+                } else {
+                    isSortByBothPressed = true;
+                    ArrayList<List> sortedList = sortByBoth(tempList);
+                    updateRecyclerView(sortedList);
+                    binding.bothButtonId.setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.purple_200));
+                }
+            }
+        });
+    }
+
+    public void fetchData(){
         FetchData data = new FetchData(this);
         data.getList(new FetchData.VolleyResponseListener() {
             @Override
@@ -65,5 +141,43 @@ public class MainActivity extends AppCompatActivity {
         adapter.setLists(list);
         binding.recyclerViewId.setAdapter(adapter);
         binding.recyclerViewId.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+    }
+
+    //Sorts the list by list id in ascending order
+    public void sortByListId(ArrayList<List> list) {
+        Collections.sort(list, new Comparator<List>() {
+            @Override
+            public int compare(List o1, List o2) {
+                return o1.getListId().compareToIgnoreCase(o2.getListId());
+            }
+        });
+    }
+
+    //Sorts the list by name in ascending order
+    public void sortByName(ArrayList<List> list) {
+        Collections.sort(list, new Comparator<List>() {
+            @Override
+            public int compare(List o1, List o2) {
+                return o1.getName().compareToIgnoreCase(o2.getName());
+            }
+        });
+    }
+
+    //Sorts the list first by list id and then by name in ascending order
+    public ArrayList<List> sortByBoth(ArrayList<List> list) {
+        sortByListId(list);
+        int listIDValue = Integer.parseInt(list.get(list.size() - 1).getListId());
+        ArrayList<List> newSortedArray = new ArrayList<>();
+        for (int i = 0; i <= listIDValue; i++) {
+            ArrayList<List> tempArray = new ArrayList<>();
+            for (List l: list) {
+                if (Integer.parseInt(l.getListId()) == i) {
+                    tempArray.add(l);
+                }
+            }
+            sortByName(tempArray);
+            newSortedArray.addAll(tempArray);
+        }
+        return newSortedArray;
     }
 }
